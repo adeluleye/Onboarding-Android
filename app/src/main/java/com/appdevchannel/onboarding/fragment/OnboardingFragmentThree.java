0ad5cbebcfc2f4788a22c1165c2b1fccce278532
adeluleye <adelu4real@gmail.com>
2018-10-27 21:04:15 +0100

    Create and implement the third OnboardingFragment class

---
+++ app/src/main/java/com/appdevchannel/onboarding/fragment/OnboardingFragmentThree.java
@@ -0,0 +1,22 @@
+package com.appdevchannel.onboarding.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appdevchannel.onboarding.R;
+
+public class OnboardingFragmentThree extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_onboarding_s3, container, false);
+
+    }
+}
