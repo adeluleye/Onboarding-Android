d9558cc3d6f4df3684ab4b441b61895c006d41ac
adeluleye <adelu4real@gmail.com>
2018-10-29 00:06:44 +0100

    Register OnboardingActivity in AndroidManifest file

--- app/src/main/java/com/appdevchannel/onboarding/activities/OnboardingActivity.java
+++ app/src/main/java/com/appdevchannel/onboarding/activities/OnboardingActivity.java
@@ -14,15 +14,16 @@ import com.vlonjatg.android.apptourlibrary.AppTour;
 public class OnboardingActivity extends AppTour {
     @Override
     public void init(@Nullable Bundle savedInstanceState) {
-        int customSlideColor = Color.parseColor("#008577");
+        int customSlideColor = Color.parseColor("#FF5722");
         addSlide(new OnboardingFragmentOne(), customSlideColor);
         addSlide(new OnboardingFragmentTwo(), customSlideColor);
         addSlide(new OnboardingFragmentThree(), customSlideColor);
-        setSeparatorColor(Color.parseColor("#008577"));
+        setSeparatorColor(Color.parseColor("#FF5722"));
         setSkipButtonTextColor(Color.WHITE);
         setNextButtonColorToWhite();
         setDoneButtonTextColor(Color.WHITE);
-        setActiveDotColor(Color.parseColor("#FF5722"));
+        setActiveDotColor(Color.parseColor("#2E2E2E"));
+        setDoneText("LOGIN");
     }
 
     @Override
